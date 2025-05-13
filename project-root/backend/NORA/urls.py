c05b0c194e7e29e18640b7af92a309dff001b6f2
AlbertoMarquillas <alberto.marquillas@studetns.salle.url.edu>
2025-05-13 23:38:09 +0200

    Eventos centrales

--- project-root/backend/NORA/urls.py
+++ project-root/backend/NORA/urls.py
@@ -20,9 +20,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('auth.urls')),
-    path('api/system/', include('system.urls')),
-    path('api/evento/', include('evento.urls')),
-    path('api/fsm_control/', include('fsm_control.urls')),
-    path('api/sensors/', include('sensors.urls')),
+    path('api/', include('auth.urls')),
+    path('api/', include('system.urls')),
+    path('api/', include('evento.urls')),
+    path('api/', include('fsm_control.urls')),
+    path('api/', include('sensors.urls')),
 ]
