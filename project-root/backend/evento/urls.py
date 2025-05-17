c822dff599087ee7833e81a97f4a3d5de7b94071
Alberto Marquillas <alberto.marquillas1017@gmail.com>
2025-05-17 15:45:37 +0200

    Cambios en las urls

--- project-root/backend/evento/urls.py
+++ project-root/backend/evento/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import recibir_evento_fsm, estado_fsm_actual
 
 urlpatterns = [
-    path("evento/", recibir_evento_fsm),
+    path("", recibir_evento_fsm),
     path("estado/", estado_fsm_actual),
 ]
