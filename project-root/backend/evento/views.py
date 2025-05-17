28d5fa775c119d5b68d8789e79a2633468e684a0
Alberto Marquillas <alberto.marquillas1017@gmail.com>
2025-05-17 15:56:54 +0200

    Cambios en el back para sincronizar (Imports)

--- project-root/backend/evento/views.py
+++ project-root/backend/evento/views.py
@@ -6,6 +6,11 @@ import json
 from fsm_control.fsm_controller import FSMController
 from evento.models import EventoRecibido
 from fsm_control.definitions.fsm_definitions import FSMEvent
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
 
 # Instancia global del controlador FSM
 fsm_controller = FSMController()
