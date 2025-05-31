1564106a97b5f6e80ff0ae739b83e6c2720a41f3
AlbertoMarquillas <alberto.marquillas@students.salle.url.edu>
2025-05-31 22:35:05 +0200

    Limpieza de rama

--- software/frontend/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import AppRoutes from "@/router";
-
-const App = () => {
-  return <AppRoutes />;
-};
-
-export default App;
--- software/frontend/src/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Merge Tailwind CSS classes with clsx logic and tailwind-merge deduplication.
- */
-export function cn(...inputs: any[]) {
-  return twMerge(clsx(...inputs));
-}
